068d7e8241052e1bc5463e9ac3deb6a8aa91c36d
TheWolfStudios154 <43331514+TheWolfStudios154@users.noreply.github.com>
2018-10-18 12:53:00 -0400

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,12 @@
+module.exports.run = (bot, message, args, discord) => {
+let emb = new Discord.RichEmbed()
+.setTitle(Bane Help System)
+.setDescription("Hello there!!")
+.addField("Coming Soon", The devs are still working on commands)
+
+message.channel.send({embed: emb})
+}
+
+module.exports.help = {
+  name: "help"
+}
