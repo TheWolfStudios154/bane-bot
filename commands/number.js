1fe61f66eca4f987c017a627e2c22dfe5262bbb6
TheWolfStudios154 <43331514+TheWolfStudios154@users.noreply.github.com>
2018-10-18 14:02:16 -0400

    Update number.js

--- commands/number.js
+++ commands/number.js
@@ -2,7 +2,7 @@ module.exports.run = (bot, message, args, Discord) => {
 let em = new Discord.RichEmbed()
 .setTitle("Bane Bots RNG")
 .setDescription("Heres a random number!")
-.addField("Number:", Math.floor(Math.random * 50000) + 1)
+.addField("Number:", `${Math.floor(Math.random * 50000)}`)
 
 message.channel.send({embed: em})
 }
