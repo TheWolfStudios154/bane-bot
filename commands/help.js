9b0c8a7c3c940ad040d1941011bc01e79c143a05
TheWolfStudios154 <43331514+TheWolfStudios154@users.noreply.github.com>
2018-10-18 14:52:29 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,6 +3,7 @@ let emb = new Discord.RichEmbed()
 .setTitle("Bane Help System")
 .setDescription("Hello there!!")
 .addField("Command lists", "Fun \nUtilty \nModeration")
+.addField("Hou to use help", "Example: b!helpfun")
 
 message.channel.send({embed: emb})
 }
