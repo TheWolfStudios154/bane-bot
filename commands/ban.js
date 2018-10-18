1ffa39efda9cccca90885301c82ba44297350007
TheWolfStudios154 <43331514+TheWolfStudios154@users.noreply.github.com>
2018-10-18 14:22:06 -0400

    Create ban.js

---
+++ commands/ban.js
@@ -0,0 +1,24 @@
+module.exports.run = (bot, message, args, Discord) => {
+ if (!message.member.hasPermission(["BAN_MEMBERS"])) return;
+    let member = message.mentions.members.first();
+    if (!member) return message.channel.send("You need to mention someone to ban.")
+    let reason = args.slice(1).join(' ');
+    if (!reason) {
+        if (member.displayName) {
+            member.ban().then(message.channel.send(`Okay, just banned ${member.displayName}.`))
+        } else {
+            member.ban().then(message.channel.send(`Okay, just banned ${member.user.username}.`))
+        }
+    } else {
+        if (member.displayName) {
+            member.ban(reason).then(message.channel.send(`Okay, just banned ${member.displayName}.`))
+        } else {
+            member.ban(reason).then(message.channel.send(`Okay, just banned ${member.user.username}.`))
+        }
+}
+
+}
+
+module.exports.help = {
+  name: "ban"
+  }
