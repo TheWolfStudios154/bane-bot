cbb6c1516b01629ceb16db06ad3d162dcf2c2553
TheWolfStudios154 <43331514+TheWolfStudios154@users.noreply.github.com>
2018-10-18 12:41:08 -0400

    Init Creation(bot.js)

---
+++ bot.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const config = require("./config.json");
+const fs = require("fs");
+var prefix = ".";
+
+bot.commands = new discord.Collection()
+
+
+fs.readdir("./commands/", (err, files) => {
+  console.log("Loading commands...");
+  if (err) return console.log(`Command loading failed!`);
+  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+  });
+});
+
+bot.on('message', message => {
+  let mArray = message.content.split(" ")
+  let args = mArray.slice(1)
+  let cmd = bot.commands.get(mArray[0].slice(prefix.length))
+  if (message.author.bot) return;
+  if (message.channel.type == "dm") return;
+  if (!message.content.startsWith(prefix)) return;
+  if (cmd) {
+    if (config.ubl.includes(message.author.id)) return;
+    cmd.run(bot, message, args, discord)
+console.log(`${message.author.username} used the ${message.content.split(" ")[0]} command.`)
+  }
+})
+
+bot.login(process.env.Token)
