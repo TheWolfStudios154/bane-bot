05dc9663eb56142e3b82a28884e66f69feba85ed
TheWolfStudios154 <43331514+TheWolfStudios154@users.noreply.github.com>
2018-10-21 15:21:08 -0400

    Create r.js

---
+++ commands/r.js
@@ -0,0 +1,35 @@
+const config = require('../config.json')
+
+module.exports.run = (bot, message, args, discord) => {
+  let em = new discord.RichEmbed()
+  .setTitle("Bane Bot Restarting...")
+  .setDescription(":wave: Restarting...")
+  .setThumbnail(bot.user.avatarURL)
+  .setTimestamp()
+  .setColor("GOLD")
+  
+  let embed = new discord.RichEmbed()
+  .setTitle("Bane Bot Restarted!")
+  .setDescription(":thumbsup: Bane Bot Successfully Restarted.")
+  .setThumbnail(bot.user.avatarURL)
+  .setTimestamp()
+  .setColor("GOLD")
+
+  function restart(channel) {
+    channel.send({embed: em})
+    .then(m => m.delete(5000))
+    .then(() => bot.destroy())
+    .then(() => bot.login(process.env.token))
+    .catch(err => console.error(err))
+    .then(() => message.channel.send({embed: embed}))
+  }
+  if (config.ownerid.includes(message.author.id)) {
+    restart(message.channel)
+  } else {
+    message.channel.send("Owner only, you cannot use this command.")
+  }
+}
+
+module.exports.help = {
+  name: "restart"
+}
