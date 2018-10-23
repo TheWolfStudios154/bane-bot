d5e3fee6cf664e7f2d1694285f54a083b36f83bf
TheWolfStudios154 <43331514+TheWolfStudios154@users.noreply.github.com>
2018-10-22 20:08:29 -0400

    Create eval.js

---
+++ commands/eval.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const ownerid = require('../config.json').ownerids
+
+module.exports.run = (bot, message, args) => {
+    if (ownerids.includes(message.author.id)) {
+    const content = message.content.split(' ').slice(1).join(' ');
+  const result = new Promise((resolve, reject) => resolve(eval(content)));
+
+  return result.then(output => {
+    if (typeof output !== 'string') output = require('util').inspect(output, { depth: 0 });
+    if (output.includes(bot.token)) output = output.replace(bot.token, 'Not for your eyes');
+    return message.channel.send(output, { code: 'js' }).then(m => m.delete(5000))
+  }).catch(err => {
+    console.error(err);
+    err = err.toString();
+
+    if (err.includes(bot.token)) err = err.replace(bot.token, 'Not for your eyes');
+
+    return message.channel.send(err, { code: 'js' })
+  }); 
+    } else {
+        message.channel.send("Owner only, you cannot use this command.")
+    }
+  }
+
+module.exports.help = {
+  name: "eval",
+  usage: ``,
+  information: "Welp"
+}
+
+module.exports.settings = {
+  permission: "Creator",
+  deleteresponder: true,
+  category: "Info"
+}
