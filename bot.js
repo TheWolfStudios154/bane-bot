6f142d1d5fe977b83ad3dd949bdb7a0569f158f0
Austin Gallippo <TheWolfStudios154@gmail.com>
2018-10-31 14:26:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const config = require("./config.json");
 const fs = require("fs");
+const item = require("./item.json")
 var prefix = "b!";
 var developer_mode = false
 
